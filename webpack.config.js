c84f3ca6f537c701491209f80539877ab63348f1
alfonsodelavega <alfonso.delavega@unican.es>
2024-09-18 16:32:50 +0200

    Add missing webpack property

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const path = require('path');
 module.exports = {
     entry: './js/app.js', // Entry point of your application
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
     },
     devServer: {
         static: ".",
         compress: true, // Enable gzip compression
-        port: 9000,
+        port: 9000
     }
 };
